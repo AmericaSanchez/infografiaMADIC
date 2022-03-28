3c40afdb1b85e2777e8b1e052e785c42949a02b5
America <americasanchezleon@gmail.com>
2022-03-27 23:30:23 -0600

    delete | quité elementos innecesarios

--- publish/web/mapa_edge.js
+++
@@ -1,9 +0,0 @@
-
-(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x4='rgba(0,0,0,0)',x9='rgba(255,255,255,1)',g='image',i='none';var g5='mapa_Mapa.svg',g8='mapa_slp.svg',g7='mapa_cdmx.svg',g6='mapa_edoMex.svg';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'mapa_Mapa',t:g,r:['41px','18px','926px','647px','auto','auto'],f:[x4,im+g5,'0px','0px']},{id:'mapa_edoMex',t:g,r:['518px','490px','61px','64px','auto','auto'],cu:'pointer',f:[x4,im+g6,'0px','0px']},{id:'mapa_cdmx',t:g,r:['556px','515px','12px','16px','auto','auto'],cu:'pointer',f:[x4,im+g7,'0px','0px']},{id:'mapa_slp',t:g,r:['458px','338px','126px','113px','auto','auto'],cu:'pointer',f:[x4,im+g8,'0px','0px']}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'1024px','679px'],overflow:'hidden',f:[x9]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-115589208");
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
-//Edge symbol: 'stage'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${mapa_edoMex}","click",function(sym,e){});
-//Edge binding end
-})("stage");
-//Edge symbol end:'stage'
-})})(AdobeEdge.$,AdobeEdge,"EDGE-115589208");
